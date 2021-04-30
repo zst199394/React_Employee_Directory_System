32b10fd57a07a9e41d0469a2b06437fc25b391d8
shuting <zst9394@outlook.com>
2021-04-29 22:38:55 -0400

    add  result

--- empdirectory/src/App.js
+++ empdirectory/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import './App.css';
+import Page from "./components/page";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Page />
+      <Footer />
     </div>
   );
 }
---
+++ empdirectory/src/components/page/index.js
@@ -0,0 +1,60 @@
+import Axios from "axios";
+import React from "react";
+
+class Content extends React.Component {
+  state = {
+    empDB: [],
+    employeesList: [],
+    search: "",
+  };
+  componentDidMount = () => {
+    Axios.get("https://randomuser.me/api/?results=200&nat=us").then(
+      (response) => {
+        console.log(response);
+        let employeeDB = response.data.results;
+        let employee = [];
+        for (let i = 0; i < employeeDB.length; i++) {
+          employee.push({
+            name: employeeDB[i].name.first + ", " + employeeDB[i].name.last,
+            email: employeeDB[i].email,
+            cell: employeeDB[i].cell,
+            picture: employeeDB[i].picture.large,
+          });
+        }
+        this.setState({
+          employeesList: employee,
+          empDB: employee,
+        });
+      }
+    );
+  };
+  render() {
+    return (
+      <div>
+        <h1>Empolyee data using RandomAPI</h1>
+        <table class="table-success">
+          <thead>
+            <tr>
+              <th scope="col">name</th>
+              <th scope="col">email</th>
+              <th scope="col">cell</th>
+              <th scope="col">pic</th>
+            </tr>
+          </thead>
+          <tbody>
+              {this.state.employeesList.map((emp,key) => (
+                  <tr>
+                      <td>{emp.name}</td>
+                      <td>{emp.email}</td>
+                      <td>{emp.cell}</td>
+                      <td><img src={emp.picture} alt={emp.name}/></td>
+                  </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Content;
